f96ace9038b7c93b8ff0aff76e9b4a0002f622da
Daniel Fritz <danielfritz@Joy-Walters-MacBook-Pro.local>
2019-01-24 11:11:25 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,26 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[2];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped" + " " + cardsInPlay[0]);
+
+var cardTwo = cards[3];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped" + " " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2){
+	//check for match
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else if (cardsInPlay[0] != cardsInPlay[1]) {
+		alert("Sorry, try again.");
+	}
+}
