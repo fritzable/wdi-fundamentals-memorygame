c96ab35f19201c853dde472956553e3543260649
Daniel Fritz <danielfritz@Joy-Walters-MacBook-Pro.local>
2019-01-24 16:09:41 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,23 +4,25 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[2];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped" + " " + cardsInPlay[0]);
-
-var cardTwo = cards[3];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped" + " " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2){
-	//check for match
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else if (cardsInPlay[0] != cardsInPlay[1]) {
 		alert("Sorry, try again.");
 	}
 }
+
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+		}
+}
+
+flipCard(3);
+flipCard(2);
+
+
+
